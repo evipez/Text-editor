3f196efaaaff519891b1cc46689871a59dfe14e5
BuildTools <unconfigured@null.spigotmc.org>
2022-08-22 19:52:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+
+import CreateTextArea from "./components/CreateTextArea";
+import ViewTextArea from "./components/ViewTextArea";
+import NotFoundPage from "./components/NotFoundPage";
+import EditTextArea from "./components/EditTextArea";
 
 function App() {
+  useEffect(() => {
+    document.title = "Text Editor";
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<CreateTextArea />} />
+
+      <Route path="posts/:id" element={<ViewTextArea />} />
+
+      <Route path="posts/:id/edit" element={<EditTextArea />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
